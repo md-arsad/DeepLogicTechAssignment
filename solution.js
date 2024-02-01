f05c894f89655d8592199ffc39a1946d9d608a0c
mdarsad <mdarsad15032001@gmail.com>
2024-02-01 19:31:31 +0530

    comment

--- solution.js
+++ solution.js
@@ -5,43 +5,54 @@ const PORT = 4000;
 
 const BASE_URL = "https://time.com";
 
+// extract the latest stories from html page
 function extractLatestStories(html) {
     const stories = [];
+// regular expression to fetch elements containing latest stories
     const regex = /<li class="latest-stories__item">(.*?)<\/li>/gs;
     let match;
+    // Looping through matches in the HTML
     while ((match = regex.exec(html)) !== null) {
         const storyHtml = match[1];
+         // Extracting headline and link using regular expressions
         const headlineMatch = storyHtml.match(/<h3 class="latest-stories__item-headline">(.*?)<\/h3>/);
         const linkMatch = storyHtml.match(/<a[^>]*href=['"](.*?)['"]/);
+
+                // If both headline and link are found, add the story to the array
+
         if (headlineMatch && linkMatch) {
             const title = headlineMatch[1].trim();
             const link = BASE_URL + linkMatch[1].trim();
             stories.push({ title, link });
         }
     }
+     // Returning the first 6 stories
     return stories.slice(0, 6);
 }
 
 const server = http.createServer((req, res) => {
-    if (req.url === "/stories" && req.method === "GET") {
-        const url = 'https://time.com';
+    // Checking if the request is for the "/stories" endpoint and it's a GET request
 
+    if (req.url === "/getTimeStories" && req.method === "GET") {
+        const url = 'https://time.com';
+ // Making an HTTPS request to the Time website
         https.get(url, (response) => {
+            
             let data = '';
 
             response.on('data', (chunk) => {
                 data += chunk;
             });
-
+ // Accumulating data as it is received
             response.on('end', () => {
                 const html = data;
-
-                const startIndex = html.indexOf('<h2 class="latest-stories__heading">');
-                const endIndex = html.indexOf('</ul>', startIndex);
-
-                const latestStoriesHtml = html.substring(startIndex, endIndex + 5);
-                const result = extractLatestStories(latestStoriesHtml);
-
+                // finding the starting of the trending section 
+                const sind = html.indexOf('<h2 class="latest-stories__heading">');
+                const endind = html.indexOf('</ul>', sind);
+                // extracting the latest 6 news 
+                const latst_stories_html = html.substring(sind, endind + 5);
+                const result = extractLatestStories(latst_stories_html);
+                // creating the response 
                 res.writeHead(200, { 'Content-Type': 'application/json' });
                 res.end(JSON.stringify(result, null, 4));
             });
